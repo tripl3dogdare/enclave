3e41b3e433703f1d66560edea4f03272a811946b
Connor Scialdone <connor@connorjulian.org>
2018-06-26 15:13:33 -0400

    Update TestBot

--- src/test/kotlin/com/tripl3dogdare/enclave/TestBot.kt
+++ src/test/kotlin/com/tripl3dogdare/enclave/TestBot.kt
@@ -1,11 +1,15 @@
 package com.tripl3dogdare.enclave
 
 import com.tripl3dogdare.enclave.event.*
+import com.tripl3dogdare.enclave.network.createMessage
+import org.http4k.format.Jackson.asJsonObject
 import java.io.File
 
+private lateinit var client:Enclave
+
 fun main(args:Array<String>) {
-  Enclave(File(".auth").readText().trim())
-    .include(TestBotEvents)
+  client = Enclave(File(".auth").readText().trim())
+  client.include(TestBotEvents)
 }
 
 object TestBotEvents : EventContainer() {
@@ -19,5 +23,10 @@ object TestBotEvents : EventContainer() {
       is MessageCreateEvent -> println(ev.raw["content"].textValue())
       is MessageDeleteEvent -> println("Message deleted.")
     }}
+
+    on<MessageCreateEvent> { ev ->
+      if(ev.raw["content"].textValue().startsWith("!ping"))
+        client.rest.createMessage(ev.raw["channel_id"].textValue(), """ {"content":"Pong!"} """.asJsonObject())
+    }
   }
 }
