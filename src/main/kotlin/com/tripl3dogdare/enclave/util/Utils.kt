0234fef8cacdf8bc53122d03ba739eb9948a28e4
Connor Scialdone <connor@connorjulian.org>
2018-06-26 14:28:42 -0400

    Add Timer-related utilities

---
+++ src/main/kotlin/com/tripl3dogdare/enclave/util/Utils.kt
@@ -0,0 +1,18 @@
+package com.tripl3dogdare.enclave.util
+
+import java.util.Timer
+import java.util.TimerTask
+
+fun Timer.setTimeout(time:Long, f:() -> Unit):TimerTask {
+  val task = TimerTask(f)
+  schedule(task, time)
+  return task
+}
+
+fun Timer.setInterval(time:Long, f:() -> Unit):TimerTask {
+  val task = TimerTask(f)
+  schedule(task, time, time)
+  return task
+}
+
+fun TimerTask(f:() -> Unit) = object:TimerTask() { override fun run() = f() }
