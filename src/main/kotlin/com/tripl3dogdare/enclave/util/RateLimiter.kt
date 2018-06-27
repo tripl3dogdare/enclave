da53c4cf51da4b08d1b9f13fcbd08b548d9d9e7a
Connor Scialdone <connor@connorjulian.org>
2018-06-26 23:09:14 -0400

    Create basic RateLimiter implementation
    - Requests to the API should now be pumped through RateLimiter#queueRequest
    - This class is not designed for general ratelimiting! It is an internal class only designed for use with the Discord API.

---
+++ src/main/kotlin/com/tripl3dogdare/enclave/util/RateLimiter.kt
@@ -0,0 +1,59 @@
+package com.tripl3dogdare.enclave.util
+
+import org.http4k.core.HttpHandler
+import org.http4k.core.Request
+import org.http4k.core.Response
+import java.time.Instant
+import java.util.*
+import org.http4k.format.Jackson.asJsonObject
+
+class RateLimiter(val httpClient:HttpHandler) {
+  val global_bucket = Bucket()
+  val buckets = mutableMapOf<String, Bucket>()
+
+  fun isLimited(path:String):Boolean =
+    global_bucket.remaining < 1 || (buckets[path]?.remaining ?: 1) < 1
+
+  fun limitFrom(path:String, resp:Response):Response {
+    if(resp.header("X-RateLimit-Global")?.toBoolean() ?: false) global_bucket.limitFrom(resp)
+    else buckets.getOrPut(path) { Bucket() }.limitFrom(resp)
+    return resp
+  }
+
+  tailrec fun queueRequest(path:String, req: Request):Response {
+    return if(isLimited(path)) {
+      val timeLeft = (buckets[path]?.resetAt ?: 0) - Instant.now().epochSecond
+      println("Request to $path is ratelimited for $timeLeft more seconds! Trying again then.")
+
+      if(timeLeft > 0) Thread.sleep(timeLeft * 1000)
+      queueRequest(path, req)
+    } else {
+      var resp = limitFrom(path, httpClient(req))
+      while(resp.status.code == 429) {
+        Thread.sleep(resp.bodyString().asJsonObject()["retry_after"].longValue())
+        resp = limitFrom(path, httpClient(req))
+      }
+      resp
+    }
+  }
+
+  companion object {
+    class Bucket {
+      var limit:Int = 0
+      var remaining:Int = 1
+      var resetAt:Long = 0
+      val timer = Timer()
+      var resetTask:TimerTask? = null
+
+      fun limitFrom(resp:Response) {
+        limit = resp.header("X-RateLimit-Limit")?.toInt() ?: 0
+        remaining = resp.header("X-RateLimit-Remaining")?.toInt() ?: 1
+        resetAt = resp.header("X-RateLimit-Reset")?.toLong() ?: 0
+
+        resetTask?.cancel()
+        resetTask = TimerTask { limit = 0; remaining = 1; resetAt = 0 }
+        timer.schedule(resetTask, Date.from(Instant.ofEpochSecond(resetAt)))
+      }
+    }
+  }
+}
