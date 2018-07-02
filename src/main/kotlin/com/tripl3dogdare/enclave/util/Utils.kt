cfc80c5143e18f52bd55fdabc50af77bf9c2a255
Connor Scialdone <connor@connorjulian.org>
2018-07-02 16:26:01 -0400

    Create Date parsing util for deserializing data objects

--- src/main/kotlin/com/tripl3dogdare/enclave/util/Utils.kt
+++ src/main/kotlin/com/tripl3dogdare/enclave/util/Utils.kt
@@ -1,7 +1,9 @@
 package com.tripl3dogdare.enclave.util
 
-import java.util.Timer
-import java.util.TimerTask
+import com.fasterxml.jackson.annotation.JsonCreator
+import java.time.Instant
+import java.time.format.DateTimeFormatter
+import java.util.*
 
 fun Timer.setTimeout(time:Long, f:() -> Unit):TimerTask {
   val task = TimerTask(f)
@@ -16,3 +18,7 @@ fun Timer.setInterval(time:Long, f:() -> Unit):TimerTask {
 }
 
 fun TimerTask(f:() -> Unit) = object:TimerTask() { override fun run() = f() }
+
+@JsonCreator fun Date.fromString(from:String):Date {
+  return Date.from(Instant.parse(from))
+}
