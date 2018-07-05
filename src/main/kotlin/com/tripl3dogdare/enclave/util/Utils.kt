de071ab62bccc2b953584aaee56b38fe9dfcb3b2
Connor Scialdone <connor@connorjulian.org>
2018-07-04 18:01:31 -0600

    Add utility functions for returning null on specific exceptions
    There's a really common pattern in the data classes at the moment of:
    
    fun fromJson(raw:Json) = 
      try { ... }
      catch(_:NullPointerException) { null }
    
    This commit sets up utility functions to abstract that away, replacing it with:
    
    fun fromJson(raw:Json):T =
      tryOrNull<NullPointerException, T> { ... }
    
    Or, more conveniently, there is a specific function for this common case:
    
    fun fromJson(raw:Json) =
      unassertNotNull { ... }

--- src/main/kotlin/com/tripl3dogdare/enclave/util/Utils.kt
+++ src/main/kotlin/com/tripl3dogdare/enclave/util/Utils.kt
@@ -15,3 +15,13 @@ fun Timer.setInterval(time:Long, f:() -> Unit):TimerTask {
 }
 
 fun TimerTask(f:() -> Unit) = object:TimerTask() { override fun run() = f() }
+
+inline fun <reified E: Exception, T> tryOrNull(f:() -> T):T? =
+  try { f() } catch(e:Exception) { when(e) {
+    is E -> null
+    else -> throw e
+  }}
+fun <T> tryOrNullAny(f:() -> T):T? =
+  tryOrNull<Exception, T>(f)
+fun <T> unassertNotNull(f:() -> T):T? =
+  tryOrNull<NullPointerException, T>(f)
